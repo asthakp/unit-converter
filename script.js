3b42832cf8e47ed2664c02287ab4bd807a0d3124
Astha <aasudagreat@gmail.com>
2023-02-05 18:05:14 +0545

    conversion complete

--- script.js
+++ script.js
@@ -14,11 +14,18 @@ const textBox=document.getElementById("box")
 const lengthConvert=document.getElementById("length")
 const volumeConvert=document.getElementById("volume")
 const massConvert=document.getElementById("mass")
-console.log(textBox)
-console.log(lengthConvert)
-console.log(volumeConvert)
-console.log(massConvert)
+
 
 convertButton.addEventListener("click", ()=>{
+const a= textBox.value
+const meterToFeet= parseFloat(a * 3.281).toFixed(2)
+const FeetToMeter= parseFloat(a/3.281).toFixed(2)
+lengthConvert.textContent=`${a} meters= ${meterToFeet} feet | ${a} feet= ${FeetToMeter} meters`
+const litreToGallon= parseFloat(a * 0.264).toFixed(2)
+const gallonToLitre=parseFloat(a / 0.264).toFixed(2)
+volumeConvert.textContent=`${a} liters= ${litreToGallon} gallons | ${a} gallons= ${gallonToLitre} liters`
+const kilosToPounds= parseFloat(a * 2.204).toFixed(2)
+const poundsToKilos=parseFloat(a / 2.204).toFixed(2)
+massConvert.textContent=`${a} kilos= ${kilosToPounds} pounds | ${a} pounds= ${poundsToKilos} kilos`
 
 })
