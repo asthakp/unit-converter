3c4dfffa34286038a6b2d3eb3b80162f759f035f
Astha <aasudagreat@gmail.com>
2023-02-05 17:26:17 +0545

    js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
 const divOne=document.getElementById("container-one")
     let text="Convert"
     let abutton=`<br><button id="btn"> ${text} </button>`
     divOne.innerHTML+=abutton
 
+const convertButton=document.getElementById("btn")
+const textBox=document.getElementById("box")
+const lengthConvert=document.getElementById("length")
+const volumeConvert=document.getElementById("volume")
+const massConvert=document.getElementById("mass")
+console.log(textBox)
+console.log(lengthConvert)
+console.log(volumeConvert)
+console.log(massConvert)
 
+convertButton.addEventListener("click", ()=>{
+
+})
